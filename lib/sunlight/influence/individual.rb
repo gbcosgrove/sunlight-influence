ef5d065d43dfb3b51cf99f143890575d34d4253d
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-02 22:17:27 -0500

    Add methods to inndividual.rb

--- lib/sunlight/influence/individual.rb
+++ lib/sunlight/influence/individual.rb
@@ -1,3 +1,25 @@
 class Sunlight::Influence::Individual
   extend CallConstructor
+
+  def self.top_individuals_by_contributions_given
+  end
+
+  def self.top_recipient_organizations
+  end
+
+  def self.top_recipient_politicians
+  end
+
+  def self.party_breakdown
+  end
+
+  def self.lobbying_registrants
+  end
+
+  def self.lobbying_clients
+  end
+
+  def self.lobbying_issues
+  end
+
 end
