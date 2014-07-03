5a705b30f5dd0b8b1b37dc217a70c5c5a18580be
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-02 22:14:05 -0500

    Add methods to politician.rb

--- lib/sunlight/influence/influence_query.rb
+++ lib/sunlight/influence/influence_query.rb
@@ -1,5 +1,31 @@
 class Sunlight::Influence::ItemizedSeach
   extend CallConstructor
 
+  def self.campaign_contributions
+  end
+
+  def self.bundled_campaign_contributions
+  end
+
+  def self.contractor_misconduct
+  end
+
+  def self.earmarks
+  end
+
+  def self.epa_echo
+  end
+
+  def self.faca_memberships
+  end
+
+  def self.federal_contracts
+  end
+
+  def self.federal_grants
+  end
+
+  def self.lobbying_registrations
+  end
 
 end
--- lib/sunlight/influence/politician.rb
+++ lib/sunlight/influence/politician.rb
@@ -1,4 +1,31 @@
 class Sunlight::Influence::Politician
   extend CallConstructor
 
+  def self.top_politicians_by_contributions_received
+  end
+
+  def self.top_contributors
+  end
+
+  def self.top_industries
+  end
+
+  def self.unknown_industries
+  end
+
+  def self.top_sectors
+  end
+
+  def self.local_breakdowns
+  end
+
+  def self.contributor_type_breakdown
+  end
+
+  def self.fec_summary
+  end
+
+  def self.fec_independent_exependitures
+  end
+
 end
