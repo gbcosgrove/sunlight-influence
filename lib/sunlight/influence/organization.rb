e1ba7b5af510cecd0350c7c999e5d1010451cc6f
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-02 22:22:16 -0500

    Add organization methods

--- lib/sunlight/influence/organization.rb
+++ lib/sunlight/influence/organization.rb
@@ -1,3 +1,55 @@
 class Sunlight::Influence::Organization
   extend CallConstructor
+
+  def self.top_organizations_by_contributions_given
+  end
+
+  def self.top_recipients
+  end
+
+  def self.pac_recipients
+  end
+
+  def self.party_breakdown
+  end
+
+  def self.state_federal_breakdown
+  end
+
+  def self.lobbying_registrants
+  end
+
+  def self.lobbying_issues
+  end
+
+  def self.bills
+  end
+
+  def self.lobbyists
+  end
+
+  def self.registrant_clients
+  end
+
+  def self.registrant_issues
+  end
+
+  def self.registrant_bills
+  end
+
+  def self.registrant_lobbyists
+  end
+
+  def self.mentions_in_regulations
+  end
+
+  def self.regulatory_submissions
+  end
+
+  def self.faca_memberships
+  end
+
+  def self.fec_summary
+  end
+
 end
