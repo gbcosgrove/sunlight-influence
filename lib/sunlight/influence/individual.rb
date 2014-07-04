15e03379ca3df01738b78212759c0cf445debadf
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-04 01:47:14 -0500

    All individual search methods working

--- lib/sunlight/influence/individual.rb
+++ lib/sunlight/influence/individual.rb
@@ -1,7 +1,10 @@
 require_relative 'call_constructor_module.rb'
+require 'pry'
 
 class Sunlight::Influence::Individual < OpenStruct
   extend CallConstructor
+
+  ## Pass in search: "name"
 
   def self.ind_id_lookup(args)
     query = Sunlight::Influence::EntitySearch
@@ -9,8 +12,11 @@ class Sunlight::Influence::Individual < OpenStruct
     entity[0]["id"]
   end
 
+
   def self.top_individuals_by_contributions_given(options)
-    options[:parameters] = string_constructor(cycle: options[:cycle])
+    if options[:cycle] != nil
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indivs/top_#{options[:limit]}"
     options = {category: options[:category], parameters: options[:parameters]}
     bar = uri_builder(options)
@@ -19,7 +25,10 @@ class Sunlight::Influence::Individual < OpenStruct
 
   def self.top_recipient_organizations(options)
     entity_id = self.ind_id_lookup(search: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle], limit: options[:limit])
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indiv/#{entity_id}/recipient_orgs"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
@@ -28,7 +37,10 @@ class Sunlight::Influence::Individual < OpenStruct
 
   def self.top_recipient_politicians(options)
     entity_id = self.ind_id_lookup(search: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle], limit: options[:limit])
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indiv/#{entity_id}/recipient_pols"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
@@ -37,7 +49,10 @@ class Sunlight::Influence::Individual < OpenStruct
 
   def self.party_breakdown(options)
     entity_id = self.ind_id_lookup(search: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle])
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indiv/#{entity_id}/recipients/party_breakdown"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
@@ -46,7 +61,10 @@ class Sunlight::Influence::Individual < OpenStruct
 
   def self.lobbying_registrants(options)
     entity_id = self.ind_id_lookup(search: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle], limit: options[:limit])
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indiv/#{entity_id}/registrants"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
@@ -55,16 +73,22 @@ class Sunlight::Influence::Individual < OpenStruct
 
   def self.lobbying_clients(options)
     entity_id = self.ind_id_lookup(search: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle], limit: options[:limit])
-    options[:category] = "aggregates/indiv/#{entity_id}/party_breakdown"
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
+    options[:category] = "aggregates/indiv/#{entity_id}/clients"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
     sunlight_call(call)
   end
 
   def self.lobbying_issues(options)
-    entity_id = self.ind_id_lookup(name: options[:name])
-    options[:parameters] = string_constructor(cycle: options[:cycle], limit: options[:limit])
+    entity_id = self.ind_id_lookup(search: options[:name])
+    options.delete(:name)
+    if !options.empty?
+      options[:parameters] = string_constructor(options)
+    end
     options[:category] = "aggregates/indiv/#{entity_id}/issues"
     args = {category: options[:category], parameters: options[:parameters]}
     call = uri_builder(args)
